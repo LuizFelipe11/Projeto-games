a0216a23e65346c6631d452c8725af71dc70f601
LuizFelipe11 <61233907+LuizFelipe11@users.noreply.github.com>
2023-12-28 14:55:51 -0300

    Primeira Versao do Projeto
    ocorreram alguns problema e foi necessário deletar o outro projeto, mas essa é a versao definitiva

---
+++ Projeto-Games/js/index.js
@@ -0,0 +1,10 @@
+function ativarMenu() {
+    const menuMobile = document.getElementById("menu-mobile")
+    
+    if(menuMobile.className === "menu-mobile-active") {
+        menuMobile.className = "menu-mobile"
+    } else {
+        menuMobile.className = "menu-mobile-active"
+    }
+
+}
